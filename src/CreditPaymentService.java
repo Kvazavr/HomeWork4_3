6edec3e853d9694092f0f5e833cda5eb0120d0e7
Elena Nekrasova <eparis@atlassian.com>
2023-02-12 21:32:51 +0400

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,12 @@
 public class CreditPaymentService {
     public int calculate(int amountCredit, int yearCount, double annualRate) {
-        int monthCount = yearCount*12;
-        double i = annualRate/12/100; // i = коэффициент для рассчета коэффициента аннуитета
-        double numerator1 = i+1;
+        int monthCount = yearCount * 12;
+        double i = annualRate / 12 / 100; // i = коэффициент для рассчета коэффициента аннуитета
+        double numerator1 = i + 1;
         double numerator2 = Math.pow(numerator1, monthCount);
-        double numerator = i*numerator2;
+        double numerator = i * numerator2;
         double denominator = numerator2 - 1;
-        double coefficient = numerator/denominator;
+        double coefficient = numerator / denominator;
         double monthlyPayment = coefficient * amountCredit;
         return (int) monthlyPayment;
 
